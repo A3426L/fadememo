4012ea04f7e870db9297d9c2df91064a24e71110
A3426L <mikan7738@gmail.com>
2025-11-06 21:36:59 +0900

    Initial Commit

---
+++ fadememo/fadememoApp.swift
@@ -0,0 +1,17 @@
+//
+//  fadememoApp.swift
+//  fadememo
+//
+//  Created by aru on 2025/11/06.
+//
+
+import SwiftUI
+
+@main
+struct fadememoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
